29e9957fe4b3da79348408cc8de7eb2eed687ca6
bhefty <billhefty@gmail.com>
2018-02-04 13:12:00 -0600

    test vars

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -84,7 +84,8 @@ module.exports = (options) => ({
     // drop any unreachable code
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        VAR_EXP: JSON.stringify(process.env.VAR_EXP)
       }
     }),
     new webpack.NamedModulesPlugin()
