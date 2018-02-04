1ec4e37df9c11e26a57c1812c4a915e5cd7ad693
bhefty <billhefty@gmail.com>
2018-02-04 15:11:09 -0600

    mock options if testing

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -1,14 +1,31 @@
 import auth0 from 'auth0-js'
 
+let config
+
+// If testing, fake the options
+if (global.process && process.env.NODE_ENV === 'test') {
+  config = {
+    domain: 'domain',
+    clientID: 'clientID',
+    redirectUri: 'redirectUri',
+    audience: 'audience',
+    responseType: 'responseType',
+    scope: 'scope'
+  }
+} else {
+  // If not, use the correct options
+  config = {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    redirectUri: process.env.AUTH0_REDIRECT_URI,
+    audience: process.env.AUTH0_AUDIENCE,
+    responseType: process.env.AUTH0_RESPONSE_TYPE,
+    scope: process.env.AUTH0_SCOPE
+  }
+}
+
 export default class Auth {
-  auth0 = new auth0.WebAuth({
-    domain: 'bhefty.auth0.com',
-    clientID: 'V6Suys2WmYNDVMjwXRDGIT1SPc0Rf48J',
-    redirectUri: 'http://localhost:3000/callback',
-    audience: 'https://bhefty.auth0.com/userinfo',
-    responseType: 'token id_token',
-    scope: 'openid profile'
-  })
+  auth0 = new auth0.WebAuth(config)
 
   constructor () {
     this.login = this.login.bind(this)
