8d071046a7340f183ab70904930e08bf188f7a32
bhefty <billhefty@gmail.com>
2018-02-12 23:28:25 -0600

    refactor acceptRequest

--- server/schema.js
+++ server/schema.js
@@ -1,5 +1,4 @@
 const makeExecutableSchema = require('graphql-tools').makeExecutableSchema
-const find = require('lodash').find
 const filter = require('lodash').filter
 const includes = require('lodash').includes
 const mongoose = require('mongoose')
@@ -8,47 +7,6 @@ const fetch = require('node-fetch')
 
 const User = require('./models/user')
 const Book = require('./models/book')
-
-const users = [
-  {
-    id: 1,
-    firstName: 'First',
-    lastName: 'Coleman',
-    email: 'abc@example.com',
-    booksInLibrary: [1, 4, 7],
-    booksUserRequested: [],
-    booksOtherRequested: []
-  }, {
-    id: 2,
-    firstName: 'Second',
-    lastName: 'Smith',
-    email: 'def@example.com',
-    booksInLibrary: [3, 8],
-    booksUserRequested: [],
-    booksOtherRequested: []
-  }, {
-    id: 3,
-    firstName: 'Third',
-    lastName: 'Knight',
-    email: 'ghi@example.com',
-    booksInLibrary: [6, 2, 10, 5],
-    booksUserRequested: [],
-    booksOtherRequested: []
-  }
-]
-
-const books = [
-  { id: '1', title: 'Book 1', author: 'Author 1', coverImg: 'cover1.jpg' },
-  { id: '2', title: 'Book 2', author: 'Author 2', coverImg: 'cover2.jpg' },
-  { id: '3', title: 'Book 3', author: 'Author 3', coverImg: 'cover3.jpg' },
-  { id: '4', title: 'Book 4', author: 'Author 4', coverImg: 'cover4.jpg' },
-  { id: '5', title: 'Book 5', author: 'Author 5', coverImg: 'cover5.jpg' },
-  { id: '6', title: 'Book 6', author: 'Author 6', coverImg: 'cover6.jpg' },
-  { id: '7', title: 'Book 7', author: 'Author 7', coverImg: 'cover7.jpg' },
-  { id: '8', title: 'Book 8', author: 'Author 8', coverImg: 'cover8.jpg' },
-  { id: '9', title: 'Book 9', author: 'Author 9', coverImg: 'cover9.jpg' },
-  { id: '10', title: 'Book 10', author: 'Author 10', coverImg: 'cover10.jpg' }
-]
 
 const typeDefs = `
   type User {
@@ -238,28 +196,38 @@ const resolvers = {
         })
     },
     acceptRequest: (_, { requesterId, ownerId, bookId }) => {
-      const requester = find(users, { id: requesterId })
-      const owner = find(users, { id: ownerId })
-      const book = find(books, { id: bookId })
+      const findRequester = User.findOne({ _id: requesterId })
+      const findOwner = User.findOne({ _id: ownerId })
+      const findBook = Book.findOne({ bookId })
 
-      if (!requester) {
-        throw new Error(`Couldn't find the requesting user with id ${requester}`)
-      }
-      if (!owner) {
-        throw new Error(`Couldn't find the owner user with id ${owner}`)
-      }
-      if (!book) {
-        throw new Error(`Couldn't find the book with id ${bookId}`)
-      }
+      return Promise.all([findRequester, findOwner, findBook])
+        .then((result) => {
+          const requester = result[0]
+          const owner = result[1]
+          const book = result[2]
 
-      // Remove book from owner's requested from other's list
-      owner.booksOtherRequested = filter(owner.booksOtherRequested, (item) => item.bookId !== bookId && item.ownerId !== ownerId)
+          if (!requester) {
+            throw new Error(`Couldn't find the requesting user with id ${requester}`)
+          }
+          if (!owner) {
+            throw new Error(`Couldn't find the owner user with id ${owner}`)
+          }
+          if (!book) {
+            throw new Error(`Couldn't find the book with id ${bookId}`)
+          }
 
-      // Remove book from requester's user request list and add it to their library
-      requester.booksUserRequested = filter(requester.booksUserRequested, (item) => item.bookId !== bookId && item.ownerId !== ownerId)
-      requester.booksInLibrary.push(bookId)
-
-      return owner
+          // Remove book from requester's user request list and add it to their library
+          return User.findOneAndUpdate({ _id: requester._id }, {
+            $pull: { booksUserRequested: { bookId: bookId, ownerId: ownerId } },
+            $push: { booksInLibrary: bookId }
+          }, { new: true })
+            .then(() => {
+              // Remove book from owner's requested from other's list
+              return User.findOneAndUpdate({ _id: owner._id }, {
+                $pull: { booksOtherRequested: { bookId: bookId, requesterId: requesterId } }
+              }, { new: true })
+            })
+        })
     }
   },
   User: {
