b41f9c83a8223afd83196649ac6a3cf4c1d94dec
bhefty <billhefty@gmail.com>
2018-02-16 17:36:20 -0600

    cleanup unused files

--- app/reducers.js
+++ app/reducers.js
@@ -2,7 +2,6 @@ import { fromJS } from 'immutable'
 import { combineReducers } from 'redux-immutable'
 import { LOCATION_CHANGE } from 'react-router-redux'
 
-import globalReducer from 'containers/App/reducer'
 import authReducer from 'auth/reducer'
 
 const routeInitialState = fromJS({
@@ -25,7 +24,6 @@ function routeReducer (state = routeInitialState, action) {
 export default function createReducer (injectedReducers) {
   return combineReducers({
     route: routeReducer,
-    global: globalReducer,
     auth: authReducer,
     ...injectedReducers
   })
