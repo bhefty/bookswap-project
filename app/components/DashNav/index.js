c135d9123a6bb67d1bc20d5df7ea7ea470606a5f
bhefty <billhefty@gmail.com>
2018-02-27 14:08:23 -0600

    add tests

--- app/components/DashNav/index.js
+++ app/components/DashNav/index.js
@@ -6,74 +6,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-
-const Wrapper = styled.div`
-  .sidebar {
-    position: sticky;
-    top: 0;
-  }
-
-  .sidebar-button-wrapper {
-      display: grid;
-      grid-template-rows: 100px;
-      grid-auto-flow: row;
-      grid-auto-rows: 100px;
-      margin: 0;
-      padding: 0;
-      align-content: center;
-      height: 100vh;
-
-      @media (max-width: 848px) {
-        grid-template-columns: 100px;
-        grid-auto-flow: column;
-        grid-auto-columns: 100px;
-        height: 100%;
-        justify-content: center;
-      }
-
-      button {
-        border: none;
-        border-bottom: 1px solid #fff;
-        @media (max-width: 848px) {
-          border-bottom: none;
-          border-right: 1px solid #fff;
-        }
-        outline: 0;
-        background: rgba(59, 171, 221, 0.5);
-        transition: background 0.3s ease-in-out;
-        color: #fff;
-        cursor: pointer;
-
-        .sidebar-context {
-          
-          svg {
-            height: 30px;
-            width: 30px;
-          }
-        }
-        &:hover {
-          background: rgba(59, 171, 221, 1);
-        }
-      }
-      button:first-child {
-        border-top-right-radius: 8px;
-        @media (max-width: 848px) {
-          border-top-right-radius: 0;
-          border-bottom-left-radius: 8px;
-        }
-      }
-
-      button:last-child {
-        border-bottom: none;
-        border-bottom-right-radius: 8px;
-        @media (max-width: 848px) {
-          border-right: none;
-          border-bottom-right-radius: 8px;
-        }
-      }
-    }
-`
+import Wrapper from './Wrapper'
 
 class DashNav extends React.Component {
   render () {
@@ -81,7 +14,7 @@ class DashNav extends React.Component {
       <Wrapper>
         <nav className='sidebar'>
           <div className='sidebar-button-wrapper'>
-            <button onClick={() => this.props.updateShowSection('AddBook')}>
+            <button id='sidebar-btn-add-book' onClick={() => this.props.updateShowSection('AddBook')}>
               <span className='sidebar-context'>
                 <svg fill='#000000' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'>
                   <path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z' />
@@ -91,21 +24,21 @@ class DashNav extends React.Component {
               <br />
               <span>ADD BOOK</span>
             </button>
-            <button onClick={() => this.props.updateShowSection('MyBooks')}>
+            <button id='sidebar-btn-my-books' onClick={() => this.props.updateShowSection('MyBooks')}>
               <span className='sidebar-context'>
                 {this.props.userInfo.booksInLibrary.length}
               </span>
               <br />
               <span>MY BOOKS</span>
             </button>
-            <button onClick={() => this.props.updateShowSection('UserRequested')}>
+            <button id='sidebar-btn-wishlist' onClick={() => this.props.updateShowSection('UserRequested')}>
               <span className='sidebar-context'>
                 {this.props.userInfo.booksUserRequested.length}
               </span>
               <br />
               <span>WISHLIST</span>
             </button>
-            <button onClick={() => this.props.updateShowSection('OtherRequested')}>
+            <button id='sidebar-btn-requests' onClick={() => this.props.updateShowSection('OtherRequested')}>
               <span className='sidebar-context'>
                 {this.props.userInfo.booksOtherRequested.length}
               </span>
--- app/components/DashNav/tests/index.test.js
+++ app/components/DashNav/tests/index.test.js
@@ -1,10 +1,51 @@
-// import React from 'react'
-// import { shallow } from 'enzyme'
+import React from 'react'
+import { shallow } from 'enzyme'
 
-// import DashNav from '../index'
+import DashNav from '../index'
 
 describe('<DashNav />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false)
+  it('should render component', () => {
+    const props = {
+      userInfo: {
+        booksInLibrary: [],
+        booksUserRequested: [],
+        booksOtherRequested: []
+      },
+      updateShowSection: jest.fn()
+    }
+    const renderedComponent = shallow(
+      <DashNav {...props} />
+    )
+    expect(renderedComponent.find('Wrapper').length).toEqual(1)
+  })
+
+  it('should handle updateShowSection function', () => {
+    const props = {
+      userInfo: {
+        booksInLibrary: [],
+        booksUserRequested: [],
+        booksOtherRequested: []
+      },
+      updateShowSection: jest.fn()
+    }
+    const renderedComponent = shallow(
+      <DashNav {...props} />
+    )
+
+    renderedComponent.find('#sidebar-btn-add-book').simulate('click')
+    expect(props.updateShowSection.mock.calls.length).toEqual(1)
+    expect(props.updateShowSection).toHaveBeenCalledWith('AddBook')
+
+    renderedComponent.find('#sidebar-btn-my-books').simulate('click')
+    expect(props.updateShowSection.mock.calls.length).toEqual(2)
+    expect(props.updateShowSection).toHaveBeenCalledWith('MyBooks')
+
+    renderedComponent.find('#sidebar-btn-wishlist').simulate('click')
+    expect(props.updateShowSection.mock.calls.length).toEqual(3)
+    expect(props.updateShowSection).toHaveBeenCalledWith('UserRequested')
+
+    renderedComponent.find('#sidebar-btn-requests').simulate('click')
+    expect(props.updateShowSection.mock.calls.length).toEqual(4)
+    expect(props.updateShowSection).toHaveBeenCalledWith('OtherRequested')
   })
 })
