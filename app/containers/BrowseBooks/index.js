f2edac960978ca523547a54728982e769a71861c
bhefty <billhefty@gmail.com>
2018-03-03 14:41:01 -0600

    add support to choose user to borrow book from

--- app/containers/BrowseBooks/index.js
+++ app/containers/BrowseBooks/index.js
@@ -10,17 +10,28 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { compose } from 'redux'
 import { graphql } from 'react-apollo'
+import filter from 'lodash/filter'
 
+import injectReducer from 'utils/injectReducer'
+import reducer from 'containers/Dashboard/reducer'
+import { makeSelectDashboard } from 'containers/Dashboard/selectors'
 import { makeSelectIsAuthenticated } from 'auth/selectors'
 import { loginRequest } from 'auth/actions'
 import LoadingIndicator from 'components/LoadingIndicator'
 import SearchForm from 'components/SearchForm'
+import RequestFromModal from 'components/RequestFromModal'
 
 import booksQuery from 'graphql/queries/booksQuery.graphql'
+import requestBook from 'graphql/mutations/requestBook.graphql'
 
 export class BrowseBooks extends React.Component {
   state = {
-    filterTitle: ''
+    filterTitle: '',
+    filteredBookResults: [],
+    filteredBookResultsWithHidden: [],
+    modalIsOpen: false,
+    selectedBookOwners: [],
+    selectedBook: null
   }
 
   static propTypes = {
@@ -28,36 +39,109 @@ export class BrowseBooks extends React.Component {
     isAuthenticated: PropTypes.bool.isRequired
   }
 
+  handleHideMyBooks = (e) => {
+    const currentBooks = this.state.filteredBookResults.length > 0 ? this.state.filteredBookResults : this.props.getBooks.books
+    if (e.target.checked) {
+      const filterResult = filter(currentBooks, (book) => {
+        const userId = this.props.userInfo.userId
+        return book.owners.find(owner => owner.userId !== userId)
+      })
+      this.setState({
+        filteredBookResultsWithHidden: filterResult
+      })
+    } else {
+      this.setState({
+        filteredBookResultsWithHidden: []
+      })
+    }
+  }
+
   handleFilterBooks = (e) => {
     e.preventDefault()
-    console.log('Filtering')
+    const filterResult = filter(this.props.getBooks.books, (book) => {
+      const filterTitle = this.state.filterTitle.toLowerCase()
+      const bookTitle = book.title.toLowerCase()
+      return bookTitle.includes(filterTitle)
+    })
+    this.setState({
+      filteredBookResults: filterResult
+    })
   }
 
   onFilterTitleChange = (e) => {
     this.setState({ filterTitle: e.target.value })
   }
 
-  handleRequestBook = (book) => {
-    console.log('request book', book)
+  handleRequestBook = (owner) => {
+    const ownerId = owner.userId
+    const requesterId = this.props.userInfo.userId
+    const bookId = this.state.selectedBook.bookId
+    this.props.requestBook({
+      variables: {
+        ownerId,
+        requesterId,
+        bookId
+      }
+    })
+    this.closeModal()
+  }
+
+  handleRequestFrom = (book) => {
+    this.setState({
+      selectedBookOwners: book.owners,
+      selectedBook: book
+    })
+    this.openModal()
   }
 
   handleRedirectToLogin = () => {
     this.props.login()
   }
 
+  openModal = () => {
+    this.setState({ modalIsOpen: true })
+  }
+
+  closeModal = () => {
+    this.setState({
+      modalIsOpen: false,
+      selectedBookOwners: [],
+      selectedBook: null
+    })
+  }
+
   render () {
     if (this.props.getBooks.loading) return <LoadingIndicator />
     return (
-      <SearchForm
-        handleSubmit={this.handleFilterBooks}
-        inputValue={this.state.filterTitle}
-        inputPlaceholder={'Browse for a book title...'}
-        onInputChange={this.onFilterTitleChange}
-        handlePrimaryAction={this.props.isAuthenticated ? this.handleRequestBook : this.handleRedirectToLogin}
-        searchResults={this.props.getBooks.books}
-        actionText={this.props.isAuthenticated ? 'Request Book' : 'Login to Request'}
-        primaryButtonClass={'btn-blue'}
-      />
+      <div>
+        <label htmlFor='filter-owned-books'>
+          Don't show books I own
+        </label>
+        <input type='checkbox' className='filter-owned-books' name='filter-owned-books' onChange={e => this.handleHideMyBooks(e)} />
+        <SearchForm
+          handleSubmit={this.handleFilterBooks}
+          inputValue={this.state.filterTitle}
+          inputPlaceholder={'Browse for a book title...'}
+          onInputChange={this.onFilterTitleChange}
+          handlePrimaryAction={
+            /* istanbul ignore next */
+            this.props.isAuthenticated ? this.handleRequestFrom : this.handleRedirectToLogin
+          }
+          searchResults={
+            /* istanbul ignore next */
+            this.state.filteredBookResultsWithHidden.length > 0 ? this.state.filteredBookResultsWithHidden
+              : this.state.filteredBookResults.length > 0 ? this.state.filteredBookResults : this.props.getBooks.books
+          }
+          actionText={this.props.isAuthenticated ? 'Request Book' : 'Login to Request'}
+          primaryButtonClass={'btn-blue'}
+        />
+        <RequestFromModal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          bookOwners={this.state.selectedBookOwners}
+          onSelectOwner={this.handleRequestBook}
+        />
+      </div>
     )
   }
 }
@@ -68,8 +152,15 @@ const withBooksQuery = graphql(
   }
 )
 
+const withRequestBooks = graphql(
+  requestBook, {
+    name: 'requestBook'
+  }
+)
+
 const mapStateToProps = createStructuredSelector({
-  isAuthenticated: makeSelectIsAuthenticated()
+  isAuthenticated: makeSelectIsAuthenticated(),
+  userInfo: makeSelectDashboard()
 })
 
 export const mapDispatchToProps = (dispatch) => ({
@@ -78,7 +169,11 @@ export const mapDispatchToProps = (dispatch) => ({
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps)
 
+const withReducer = injectReducer({ key: 'dashboard', reducer })
+
 export default compose(
+  withReducer,
   withConnect,
-  withBooksQuery
+  withBooksQuery,
+  withRequestBooks
 )(BrowseBooks)
--- app/containers/BrowseBooks/tests/index.test.js
+++ app/containers/BrowseBooks/tests/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import { BrowseBooks, mapDispatchToProps } from '../index'
 import { loginRequest } from 'auth/actions'
@@ -35,9 +35,199 @@ describe('<BrowseBooks />', () => {
     expect(renderedComponent.find('LoadingIndicator').length).toEqual(1)
   })
 
+  it('should redirect if user is not logged in', () => {
+    const props = {
+      login: jest.fn(),
+      isAuthenticated: false,
+      getBooks: {
+        loading: false,
+        books: [{
+          bookId: 'book1234',
+          title: 'Title',
+          authors: ['Jimmy'],
+          description: 'Book duh',
+          coverImg: 'test.jpg',
+          owners: [{
+            userId: 'test|0987',
+            name: 'Bill'
+          }]
+        }, {
+          bookId: 'book0987',
+          title: 'Title Here',
+          authors: ['Joe'],
+          description: 'Another book',
+          coverImg: 'test2.jpg',
+          owners: [{
+            userId: 'not|me',
+            name: 'John'
+          }]
+        }]
+      }
+    }
+    const renderedComponent = mount(
+      <BrowseBooks {...props} />
+    )
+    renderedComponent.find('.btn-blue').first().simulate('click')
+    expect(props.login).toHaveBeenCalled()
+  })
+
+  it('should render action buttons with appropriate text if not logged in', () => {
+    const props = {
+      login: jest.fn(),
+      isAuthenticated: false,
+      getBooks: {
+        loading: false,
+        books: [{
+          bookId: 'book1234',
+          title: 'Title',
+          authors: ['Jimmy'],
+          description: 'Book duh',
+          coverImg: 'test.jpg',
+          owners: [{
+            userId: 'test|0987',
+            name: 'Bill'
+          }]
+        }, {
+          bookId: 'book0987',
+          title: 'Title Here',
+          authors: ['Joe'],
+          description: 'Another book',
+          coverImg: 'test2.jpg',
+          owners: [{
+            userId: 'not|me',
+            name: 'John'
+          }]
+        }]
+      }
+    }
+    const renderedComponent = mount(
+      <BrowseBooks {...props} />
+    )
+    expect(renderedComponent.find('.btn-blue').first().text()).toEqual('Login to Request')
+  })
+
+  it('should render action buttons with appropriate text if logged in', () => {
+    const props = {
+      login: jest.fn(),
+      isAuthenticated: true,
+      getBooks: {
+        loading: false,
+        books: [{
+          bookId: 'book1234',
+          title: 'Title',
+          authors: ['Jimmy'],
+          description: 'Book duh',
+          coverImg: 'test.jpg',
+          owners: [{
+            userId: 'test|0987',
+            name: 'Bill'
+          }]
+        }, {
+          bookId: 'book0987',
+          title: 'Title Here',
+          authors: ['Joe'],
+          description: 'Another book',
+          coverImg: 'test2.jpg',
+          owners: [{
+            userId: 'not|me',
+            name: 'John'
+          }]
+        }]
+      }
+    }
+    const renderedComponent = mount(
+      <BrowseBooks {...props} />
+    )
+    expect(renderedComponent.find('.btn-blue').first().text()).toEqual('Request Book')
+  })
+
   describe('handleFilterBooks', () => {
-    it.skip('should filter books based on search', () => {
-      expect(true).toEqual(false)
+    it('should return all books with no filter', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: false,
+          books: [{
+            bookId: 'book1234',
+            title: 'Title',
+            owners: [{
+              userId: 'test|0987',
+              name: 'Bill'
+            }]
+          }, {
+            bookId: 'book0987',
+            title: 'Title Here',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }]
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        handleHideMyBooks: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      const fixture = {
+        preventDefault: jest.fn()
+      }
+
+      expect(renderedComponent.state().filteredBookResults).toEqual([])
+      renderedComponent.instance().handleFilterBooks(fixture)
+      expect(renderedComponent.state().filteredBookResults).toEqual(props.getBooks.books)
+    })
+
+    it('should filter books based on search', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: false,
+          books: [{
+            bookId: 'book1234',
+            title: 'Title',
+            owners: [{
+              userId: 'test|0987',
+              name: 'Bill'
+            }]
+          }, {
+            bookId: 'book0987',
+            title: 'Title Here',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }, {
+            bookId: 'book1653',
+            title: 'The Hobbit',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }]
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        handleHideMyBooks: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      const fixture = {
+        preventDefault: jest.fn()
+      }
+
+      expect(renderedComponent.state().filteredBookResults).toEqual([])
+      renderedComponent.setState({ filterTitle: 'hobbit' })
+      renderedComponent.instance().handleFilterBooks(fixture)
+      expect(renderedComponent.state().filteredBookResults[0]).toEqual(props.getBooks.books[2])
     })
   })
 
@@ -69,8 +259,37 @@ describe('<BrowseBooks />', () => {
   })
 
   describe('handleRequestBook', () => {
-    it.skip('should request book from owner', () => {
-      expect(true).toEqual(false)
+    it('should request book from owner', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: true,
+          books: []
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        requestBook: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      renderedComponent.setState({
+        selectedBook: {
+          bookId: 'book5432',
+          title: 'Title'
+        }
+      })
+
+      const fixture = {
+        userId: 'test|1234',
+        name: 'John Smith'
+      }
+
+      renderedComponent.instance().handleRequestBook(fixture)
+      expect(props.requestBook).toHaveBeenCalled()
     })
   })
 
@@ -90,6 +309,296 @@ describe('<BrowseBooks />', () => {
 
       renderedComponent.instance().handleRedirectToLogin()
       expect(props.login).toHaveBeenCalled()
+    })
+  })
+
+  describe('handleHideMyBooks', () => {
+    it('should filter based on filteredBookResults if > 0', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: false,
+          books: [{
+            bookId: 'book1234',
+            title: 'Title',
+            owners: [{
+              userId: 'test|0987',
+              name: 'Bill'
+            }]
+          }, {
+            bookId: 'book0987',
+            title: 'Title Here',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }, {
+            bookId: 'book1653',
+            title: 'The Hobbit',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }, {
+            bookId: 'book0000',
+            title: 'The Hobbit: There and Back Again',
+            owners: [{
+              userId: 'test|0987',
+              name: 'Bill'
+            }]
+          }]
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        handleHideMyBooks: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+      renderedComponent.setState({
+        filteredBookResults: [{
+          bookId: 'book0987',
+          title: 'Title Here',
+          owners: [{
+            userId: 'not|me',
+            name: 'John'
+          }]
+        }, {
+          bookId: 'book1653',
+          title: 'The Hobbit',
+          owners: [{
+            userId: 'not|me',
+            name: 'John'
+          }]
+        }, {
+          bookId: 'book0000',
+          title: 'The Hobbit: There and Back Again',
+          owners: [{
+            userId: 'test|0987',
+            name: 'Bill'
+          }]
+        }]
+      })
+
+      renderedComponent.find('.filter-owned-books').simulate('change', { target: { checked: true } })
+
+      const expectedResult = [{
+        bookId: 'book0987',
+        title: 'Title Here',
+        owners: [{
+          userId: 'not|me',
+          name: 'John'
+        }]
+      }, {
+        bookId: 'book1653',
+        title: 'The Hobbit',
+        owners: [{
+          userId: 'not|me',
+          name: 'John'
+        }]
+      }]
+
+      expect(renderedComponent.state().filteredBookResultsWithHidden).toEqual(expectedResult)
+    })
+
+    it('should change state of filteredBookResultsWithHidden if check', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: false,
+          books: [{
+            bookId: 'book1234',
+            title: 'Title',
+            owners: [{
+              userId: 'test|0987',
+              name: 'Bill'
+            }]
+          }, {
+            bookId: 'book0987',
+            title: 'Title Here',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }]
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        handleHideMyBooks: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      renderedComponent.find('.filter-owned-books').simulate('change', { target: { checked: true } })
+      const expectedResult = [{
+        bookId: 'book0987',
+        title: 'Title Here',
+        owners: [{
+          userId: 'not|me',
+          name: 'John'
+        }]
+      }]
+
+      expect(renderedComponent.state().filteredBookResultsWithHidden).toEqual(expectedResult)
+    })
+
+    it('should change state of filteredBookResultsWithHidden if uncheck', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: false,
+          books: [{
+            bookId: 'book1234',
+            title: 'Title',
+            owners: [{
+              userId: 'test|0987',
+              name: 'Bill'
+            }]
+          }, {
+            bookId: 'book0987',
+            title: 'Title Here',
+            owners: [{
+              userId: 'not|me',
+              name: 'John'
+            }]
+          }]
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        handleHideMyBooks: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      renderedComponent.find('.filter-owned-books').simulate('change', { target: { checked: false } })
+      const expectedResult = []
+
+      expect(renderedComponent.state().filteredBookResultsWithHidden).toEqual(expectedResult)
+    })
+  })
+
+  describe('openModal', () => {
+    it('should change state of modalIsOpen', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: true,
+          books: []
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        openModal: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      expect(renderedComponent.state().modalIsOpen).toEqual(false)
+      renderedComponent.instance().openModal()
+      expect(renderedComponent.state().modalIsOpen).toEqual(true)
+    })
+  })
+
+  describe('closeModal', () => {
+    it('should change state of modalIsOpen', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: true,
+          books: []
+        },
+        userInfo: {
+          userId: 'test|0987'
+        },
+        closeModal: jest.fn()
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+      renderedComponent.setState({ modalIsOpen: true })
+
+      expect(renderedComponent.state().modalIsOpen).toEqual(true)
+      renderedComponent.instance().closeModal()
+      expect(renderedComponent.state().modalIsOpen).toEqual(false)
+    })
+  })
+
+  describe('handleRequestFrom', () => {
+    it('should change state of selectedBookOwners and selectedBook', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: true,
+          books: []
+        },
+        userInfo: {
+          userId: 'test|0987'
+        }
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      const fixture = {
+        bookId: 'book1234',
+        title: 'Book Title',
+        owners: [{
+          userId: 'test|1234',
+          name: 'Bill'
+        }]
+      }
+
+      expect(renderedComponent.state().selectedBookOwners).toEqual([])
+      expect(renderedComponent.state().selectedBook).toEqual(null)
+
+      renderedComponent.instance().handleRequestFrom(fixture)
+
+      expect(renderedComponent.state().selectedBookOwners).toEqual(fixture.owners)
+      expect(renderedComponent.state().selectedBook).toEqual(fixture)
+    })
+
+    it('should call openModal', () => {
+      const props = {
+        login: jest.fn(),
+        isAuthenticated: false,
+        getBooks: {
+          loading: true,
+          books: []
+        },
+        userInfo: {
+          userId: 'test|0987'
+        }
+      }
+      const renderedComponent = shallow(
+        <BrowseBooks {...props} />
+      )
+
+      const fixture = {
+        bookId: 'book1234',
+        title: 'Book Title',
+        owners: [{
+          userId: 'test|1234',
+          name: 'Bill'
+        }]
+      }
+
+      const mockOpenModal = jest.spyOn(renderedComponent.instance(), 'handleRequestFrom')
+
+      renderedComponent.instance().handleRequestFrom(fixture)
+
+      expect(mockOpenModal).toHaveBeenCalled()
     })
   })
 
