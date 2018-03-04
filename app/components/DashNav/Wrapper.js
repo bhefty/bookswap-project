35bda8fc261d1ee687116b46a80b33786648bdb3
bhefty <billhefty@gmail.com>
2018-03-04 13:06:37 -0600

    make dashnav more responsive

--- app/components/DashNav/Wrapper.js
+++ app/components/DashNav/Wrapper.js
@@ -4,9 +4,8 @@ export default styled.div`
   .sidebar {
     position: sticky;
     top: 0;
-  }
 
-  .sidebar-button-wrapper {
+    .sidebar-button-wrapper {
       display: grid;
       grid-template-rows: 100px;
       grid-auto-flow: row;
@@ -16,23 +15,11 @@ export default styled.div`
       align-content: center;
       height: 100vh;
 
-      @media (max-width: 848px) {
-        grid-template-columns: 100px;
-        grid-auto-flow: column;
-        grid-auto-columns: 100px;
-        height: 100%;
-        justify-content: center;
-      }
-
       button {
         border: none;
         border-bottom: 1px solid #fff;
-        @media (max-width: 848px) {
-          border-bottom: none;
-          border-right: 1px solid #fff;
-        }
         outline: 0;
-        background: rgba(${props => props.theme.lightBlue}, 0.8);
+        background: rgba(${props => props.theme.lightBlue}, 1);
         transition: background 0.3s ease-in-out;
         color: #fff;
         cursor: pointer;
@@ -45,24 +32,102 @@ export default styled.div`
           }
         }
         &:hover {
-          background: rgba(${props => props.theme.lightBlue}, 1);
+          background: rgba(${props => props.theme.blue}, 1);
         }
       }
       button:first-child {
         border-top-right-radius: 8px;
-        @media (max-width: 848px) {
-          border-top-right-radius: 0;
-          border-bottom-left-radius: 8px;
-        }
       }
 
       button:last-child {
         border-bottom: none;
         border-bottom-right-radius: 8px;
-        @media (max-width: 848px) {
-          border-right: none;
-          border-bottom-right-radius: 8px;
+      }
+    }
+
+    [aria-controls="dash-list"] {
+      display: none;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .sidebar {
+      position: fixed;
+      top: -100px;
+      z-index: 1;
+      [aria-controls="dash-list"] {
+        display: block;
+        position: relative;
+        top: calc(50vh + 48px);
+        color: #fff;
+        border: none;
+        border-radius: 0 50px 50px 0;
+        background-color: rgb(${props => props.theme.lightBlue});
+        width: 50px;
+        height: 100px;
+        outline: 0;
+
+        svg {
+          margin-left: -10px;
         }
       }
     }
+
+    [aria-expanded='false'] ~ .sidebar-button-wrapper {
+      max-height: 0;
+      max-width: 0;
+      overflow: hidden;
+      visibility: hidden;
+      opacity: 0;
+      z-index: -1;
+      transform: translateX(-20%);
+      transition: all 0.3s ease-in-out 0s, visibility 0s linear 0.3s, z-index 0s linear 0.01s;      
+    }
+
+    [aria-expanded='true'] ~ .sidebar-button-wrapper {
+      visibility: visible;
+      opacity: 1;
+      z-index: 1;
+      max-height: 100%;
+      max-width: 100px;
+
+      display: grid;
+      grid-template-rows: 100px;
+      grid-auto-flow: row;
+      grid-auto-rows: 100px;
+      margin: 0;
+      padding: 0;
+      align-content: center;
+      height: 100vh;
+      transition: all 0.3s ease-in-out 0s, visibility 0s linear 0.3s, z-index 0s linear 0.01s; 
+      transform: translateX(0%);
+      transition-delay: 0s, 0s, 0.3s;
+    }
+
+    [aria-expanded='false'] .dash-close {
+      display: none;
+    }
+
+    [aria-expanded='true'] .dash-close {
+      display: inline-block;
+      position: relative;
+      left: 88px;
+      color: #fff;
+      border: none;
+      border-radius: 0 50px 50px 0;
+      background-color: rgb(${props => props.theme.blue});
+      width: 50px;
+      height: 100px;
+      line-height: 100px;
+      outline: 0;
+
+      svg {
+        margin-left: -10px;
+      }
+    }
+
+    [aria-expanded='true'] .dash-open {
+      display: none;
+    }
+  }
 `
--- app/components/DashNav/index.js
+++ app/components/DashNav/index.js
@@ -9,11 +9,31 @@ import PropTypes from 'prop-types'
 import Wrapper from './Wrapper'
 
 class DashNav extends React.Component {
+  state = { expanded: false }
+
+  toggleNav = () => {
+    this.setState({ expanded: !this.state.expanded })
+  }
+
   render () {
     return (
       <Wrapper>
         <nav className='sidebar'>
-          <div className='sidebar-button-wrapper'>
+          <button className='dash-toggle' aria-expanded={this.state.expanded} aria-controls='dash-list' onClick={this.toggleNav}>
+            <span className='dash-open'>
+              <svg fill='#FFFFFF' height='40' viewBox='0 0 24 24' width='40' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z' />
+                <path d='M0-.25h24v24H0z' fill='none' />
+              </svg>
+            </span>
+            <span className='dash-close'>
+              <svg fill='#FFFFFF' height='40' viewBox='0 0 24 24' width='40' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z' />
+                <path d='"M0-.5h24v24H0z' fill='none' />
+              </svg>
+            </span>
+          </button>
+          <div id='dash-list' className='sidebar-button-wrapper'>
             <button id='sidebar-btn-add-book' onClick={() => this.props.updateShowSection('AddBook')}>
               <span className='sidebar-context'>
                 <svg fill='#000000' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'>
